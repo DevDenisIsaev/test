60d1d23087af1ce607f0cd3d2328ec4d4c2daf15
vagrant <vagrant@precise32.(none)>
2014-11-13 11:01:05 +0400

    Test5

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Project::Application.config.secret_token = '4d43e514fd4b381a506be37b4d8fad1a6ffd5256db297b400d948f1386e2cb783d8cad4b94ac3d49f6315df7bf4d9c1a26313cd8ad73ac9c230c4b7787391057'
+Admin::Application.config.secret_token = '4d43e514fd4b381a506be37b4d8fad1a6ffd5256db297b400d948f1386e2cb783d8cad4b94ac3d49f6315df7bf4d9c1a26313cd8ad73ac9c230c4b7787391057'
