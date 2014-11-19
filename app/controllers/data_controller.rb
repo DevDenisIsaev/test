732910a069b45f892d5ad0d4020b7a1080810218
vagrant <vagrant@precise32.(none)>
2014-11-19 17:14:43 +0400

    Sort

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -2,9 +2,13 @@ class DataController < ApplicationController
   before_filter :signed_in_user
 
   def index
-
+    @param = params[:field]
+    sqlorder = ""
+    if (!@param.nil?)
+      sqlorder = " ORDER BY " + @param + " DESC"
+    end
     @connection = ActiveRecord::Base.establish_connection :remote_db
-    sql = "select * from v_xml_cdr ORDER BY start_stamp DESC"
+    sql = "select * from v_xml_cdr" + sqlorder
     @result = @connection.connection.execute(sql)
     ActiveRecord::Base.establish_connection
 
